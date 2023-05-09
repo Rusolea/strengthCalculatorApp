5ad0b99ed5a770d4ba61826e98fa2c54878cbdf1
Rusolea <Tu direccin de correo electrnico>
2023-05-09 13:37:29 -0300

    Agrega nuevos ejercicios al selector y actualiza cálculo de tabla de fuerza
    - Añade nuevos ejercicios al selector, incluyendo press de pecho de suelo con barra, remo a un brazo, estocadas, peso muerto, y otros.
    - Actualiza la fórmula de la tabla de fuerza para usar el conjunto de multiplicadores personalizado.
    - Ajusta los pesos en la tabla de fuerza para mostrarlos en intervalos de 2,5 kg, y en intervalos de 1 kg si el resultado es menor a 10 kg.

--- src/app/strength-calculator/ui-components/exercise-selection-modal/exercise-selection-modal.component.ts
+++ src/app/strength-calculator/ui-components/exercise-selection-modal/exercise-selection-modal.component.ts
@@ -8,10 +8,26 @@ import { Component, Output, EventEmitter } from '@angular/core';
 export class ExerciseSelectionModalComponent {
   @Output() exerciseChange = new EventEmitter<string>();
   exercises: { id: string; name: string }[] = [
-    { id: 'squat', name: 'Sentadilla' },
+   /*  { id: 'squat', name: 'Sentadilla' },
     { id: 'bench_press', name: 'Press de banca' },
-    { id: 'hip_thrust', name: 'Hip thrust' },
+    { id: 'hip_thrust', name: 'Hip thrust' }, */
     // Agrega más ejercicios aquí
+  { id: 'squat', name: 'Sentadilla' },
+  { id: 'box_squat', name: 'Media sentadilla con cajón' },
+  { id: 'bench_press', name: 'Press de banca' },
+  { id: 'hip_thrust', name: 'Hip thrust' },
+  { id: 'single_leg_hip_thrust', name: 'Hip thrust a una pierna' },
+  { id: 'push_press', name: 'Push press' },
+  { id: 'barbell_curl', name: 'Curl de bíceps con barra' },
+  { id: 'barbell_shoulder_press', name: 'Press de hombros con barra' },
+  { id: 'tricep_extension', name: 'Extensión de tríceps con mancuerna tras nuca' },
+  { id: 'pull_up', name: 'Dominadas' },
+  { id: 'dumbbell_curl', name: 'Curl de bíceps con mancuernas' },
+  { id: 'one_arm_dumbbell_row', name: 'Remo a un brazo con mancuerna' },
+  { id: 'lunges', name: 'Estocadas' },
+  { id: 'deadlift', name: 'Peso muerto' },
+  { id: 'barbell_floor_chest_press', name: 'Press de pecho de suelo con barra' },
+
   ];
   selectedExercise: string = 'squat';
 
