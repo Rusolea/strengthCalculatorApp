d2b667977ec55a2da40e42ca2b65564754fef68c
Rusolea <Tu direccin de correo electrnico>
2023-05-09 13:23:18 -0300

    Actualizar fórmula de 1RM y ajustar tabla de fuerza
    - Se ha actualizado la fórmula de 1RM para utilizar la proporcionada por el usuario.
    - Se ha ajustado la función generateStrengthTable para utilizar los nuevos multiplicadores.
    - La tabla de fuerza ahora muestra los pesos en intervalos de 2.5 kg y 1 kg (cuando el resultado es menor a 10 kg).

--- src/app/strength-calculator/services/strength-calculator.service.ts
+++ src/app/strength-calculator/services/strength-calculator.service.ts
@@ -13,7 +13,7 @@ export class StrengthCalculatorService {
   }
 
 
- // Función para generar una tabla de pesos y repeticiones
+ /* // Función para generar una tabla de pesos y repeticiones
  generateStrengthTable(oneRepMax: number): { reps: number, percentage: number, weight: number }[] {
   const table = [];
   for (let reps = 1; reps <= 12; reps++) {
@@ -23,6 +23,76 @@ export class StrengthCalculatorService {
   }
   return table;
 }
+ */
+/* generateStrengthTable(oneRM: number) {
+  const multipliers = [
+    1,
+    0.95,
+    0.9,
+    0.88,
+    0.86,
+    0.83,
+    0.8,
+    0.78,
+    0.76,
+    0.75,
+    0.72,
+    0.7,
+    0.62,
+    0.58,
+    0.56,
+  ];
+
+  const strengthTable = multipliers.map((multiplier, index) => {
+    const weight = oneRM * multiplier;
+    const repetitions = index + 1;
+    return {
+      percentage: Math.round(multiplier * 100) + "%",
+      weight: Math.round(weight) + " kg",
+      repetitions: repetitions,
+    };
+  });
+
+  return strengthTable;
+}
 
 
+}
+ */
+generateStrengthTable(oneRM: number) {
+  const multipliers = [
+    1,
+    0.95,
+    0.9,
+    0.88,
+    0.86,
+    0.83,
+    0.8,
+    0.78,
+    0.76,
+    0.75,
+    0.72,
+    0.7,
+    0.62,
+    0.58,
+    0.56,
+  ];
+
+  const roundToInterval = (value: number, interval: number) => {
+    return Math.round(value / interval) * interval;
+  };
+
+  const strengthTable = multipliers.map((multiplier, index) => {
+    let weight = oneRM * multiplier;
+    weight = weight < 10 ? roundToInterval(weight, 1) : roundToInterval(weight, 2.5);
+    const repetitions = index + 1;
+    return {
+      percentage: Math.round(multiplier * 100) + "%",
+      weight: weight + " kg",
+      repetitions: repetitions,
+    };
+  });
+
+  return strengthTable;
+};
 }
