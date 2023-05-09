9e7d4fd84aad4f32d26124660786f401b0278a19
Rusolea <Tu direccin de correo electrnico>
2023-05-09 18:36:55 -0300

    "Implementación y personalización de SCSS. Se agregaron estilos globales y se ajustaron los estilos de los componentes. Se corrigieron problemas de la tabla de cálculo de fuerza y se ajustó el orden de las columnas."

--- src/app/strength-calculator/services/strength-calculator.service.ts
+++ src/app/strength-calculator/services/strength-calculator.service.ts
@@ -59,7 +59,7 @@ export class StrengthCalculatorService {
 
 }
  */
-generateStrengthTable(oneRM: number) {
+/* generateStrengthTable(oneRM: number) {
   const multipliers = [
     1,
     0.95,
@@ -95,4 +95,41 @@ generateStrengthTable(oneRM: number) {
 
   return strengthTable;
 };
+} */
+generateStrengthTable(oneRM: number) {
+  const multipliers = [
+    1,
+    0.95,
+    0.9,
+    0.88,
+    0.86,
+    0.83,
+    0.8,
+    0.78,
+    0.76,
+    0.75,
+    0.72,
+    0.7,
+    0.62,
+    0.58,
+    0.56,
+  ];
+
+  const roundToInterval = (value: number, interval: number) => {
+    return Math.round(value / interval) * interval;
+  };
+
+  const strengthTable = multipliers.map((multiplier, index) => {
+    let weight = oneRM * multiplier;
+    weight = weight < 10 ? roundToInterval(weight, 1) : roundToInterval(weight, 2.5);
+    return {
+      percentage: Math.round(multiplier * 100), // removido "%"
+      weight: weight, // removido " kg"
+      reps: index + 1, // agregado "reps"
+    };
+  });
+
+  return strengthTable;
 }
+};
+
